77c41b0b8c01a39d96ae0c992effa7195f773d3b
thangmo29 <worapat29@gmail.com>
2022-07-27 18:35:47 +0700

    Update SourceCode

---
+++ Sourecode/Source/Trashman/ControlPointer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ControlPointer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRASHMAN_API UControlPointer : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
